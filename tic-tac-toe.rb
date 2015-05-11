d6fd7396e0669247458cb9ad19d361b6dbc5f8c1
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-05-10 20:19:31 -0400

    stopping point

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -6,8 +6,8 @@ require 'pry'
 
 @xo = ['X', 'O']
 
-@x = []
-@o = []
+@player1 = []
+@player2 = []
 
 def tictoe()
   puts @table1.to_s
@@ -19,10 +19,12 @@ def user1_choice(options)
   puts 'what team player 1?'
   xoro = gets.chomp.upcase
   if xoro == "X"
-    options[0]
+    @player1 = options[0]
+    @player2 = options[1]
     puts 'player 2 is O.'
   elsif xoro == "O"
-    options[1]
+    @player1 = options[1]
+    @player2 = options[0]
     puts 'player 2 is X.'
   else
     puts 'must pick X or O.'
@@ -30,17 +32,13 @@ def user1_choice(options)
 end
 
 def gamewinner()
-  if @table1[0] == 'X'
+  if @table1[0] == 'X' || 'O'
     puts 'game over'
   end
 end
 
 def game()
-  if @table1[0] == 'X'
-    puts 'game over'
-  elsif @table1[0] == 1
     answers1
-  end
 end
 
 def prompt(options)
@@ -62,55 +60,67 @@ end
 def answers2()
 user2_input = gets.chomp.to_s
   if user2_input == "1"
-    @table1[0] = "O"
+    @table1[0] = @player2
   elsif user2_input == "2"
-    @table1[1] = "O"
+    @table1[1] = @player2
   elsif user2_input == "3"
-    @table1[2] = "O"
+    @table1[2] = @player2
   elsif user2_input == "4"
-    @table2[0] = "O"
+    @table2[0] = @player2
   elsif user2_input == "5"
-    @table2[1] = "O"
+    @table2[1] = @player2
   elsif user2_input == "6"
-    @table2[2] = "O"
+    @table2[2] = @player2
   elsif user2_input == "7"
-    @table3[0] = "O"
+    @table3[0] = @player2
   elsif user2_input == "8"
-    @table3[1] = "O"
+    @table3[1] = @player2
   elsif user2_input == "9"
-    @table3[2] = "O"
+    @table3[2] = @player2
   end
   game
 end
 
 def answers1()
 tictoe
+puts 'player 1 turn'
 user1_input = gets.chomp.to_s
   if user1_input == "1"
-    @table1[0] = "X"
+    @table1[0] = @player1
   elsif user1_input == "2"
-    @table1[1] = "X"
+    @table1[1] = @player1
   elsif user1_input == "3"
-    @table1[2] = "X"
+    @table1[2] = @player1
   elsif user1_input == "4"
-    @table2[0] = "X"
+    @table2[0] = @player1
   elsif user1_input == "5"
-    @table2[1] = "X"
+    @table2[1] = @player1
   elsif user1_input == "6"
-    @table2[2] = "X"
+    @table2[2] = @player1
   elsif user1_input == "7"
-    @table3[0] = "X"
+    @table3[0] = @player1
   elsif user1_input == "8"
-    @table3[1] = "X"
+    @table3[1] = @player1
   elsif user1_input == "9"
-    @table3[2] = "X"
+    @table3[2] = @player1
   end
   tictoe
+  puts 'player 2 turn'
   answers2
 end
 
+def game_winner()
+  if @table1 == ['X', 'X', 'X']
+    puts "Winner X"
+  elsif @table2 == ['X', 'X', 'X']
+    puts "Winner X"
+  elsif @table3 == ['X', 'X', 'X']
+    puts "Winner X"
+  end
+end
 
 
+game_winner
 prompt(@xo)
 
 binding.pry
