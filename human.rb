5d68210a633f398c32260b58fec1ca93a35f4d61
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-05-12 19:43:17 -0400

    human with questions for class

---
+++ human.rb
@@ -0,0 +1,31 @@
+class Human
+  def initialize(name)
+    @name = name
+    @alertness = 0.0
+    @flavor = Coffee.new(name)
+    @coffee = 0
+  end
+
+  def alertness
+    @alertness
+  end
+
+  def has_coffee?()
+    false
+  end
+
+  def needs_coffee?()
+    true
+  end
+
+  def drink!()
+    @alertness += 0.31
+    @coffee -= 0.34
+  end
+
+  def buy(type)
+    @flavor
+    @coffee += 1 #add tsmf to some kind of array or some shit
+  end
+
+end
