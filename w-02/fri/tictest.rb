fdcd45fd95cfb32dba17f9ceff37e8757affe5bf
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-05-18 18:30:46 -0400

    still not what I want

--- w-02/fri/tictest.rb
+++ w-02/fri/tictest.rb
@@ -8,4 +8,14 @@ class Tictest < MiniTest::Test
     assert win?(@table) != draw?(@table)
   end
 
+  def test_player
+    assert_equal player1, 'X'
+    assert_equal player2, 'O'
+  end
+
+  def test_input
+    assert_equal input, '1'
+    hum_v
+  end
+
 end
